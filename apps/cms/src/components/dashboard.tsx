ac164bbd81a5cbaa91b4f56a16256fab84b3e671
Rahul Yadav <rahul.yadav@codinova.com>
2024-07-22 01:16:16 +0530

    use card and icon components from repo/ui

--- apps/cms/src/components/dashboard.tsx
+++ apps/cms/src/components/dashboard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Card from "@repo/ui/card";
+import { Card, Icon, Icons, IconSize } from "@repo/ui";
 // import DatePickerInput from '@repo/ui/datepicker';
 
 function Dashboard() {
@@ -9,7 +9,8 @@ function Dashboard() {
       <div className="gutter--left gutter--right dashboard__wrap">
         <div className="dashboard__group">
           <h2 className="dashboard__label pl-8">Dashboard</h2>
-          <Card />
+          <Card heading='Card' type='primary' />
+          <Icon name={Icons.BULB_ICON} size={IconSize.sm} cssClasses='pl-8' />
           {/* <DatePickerInput></DatePickerInput> */}
         </div>
       </div>
