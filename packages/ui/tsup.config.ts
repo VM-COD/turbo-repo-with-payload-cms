545429eb07ec1d22a423ea0d74d775790a8d666f
Rahul Yadav <rahul.yadav@codinova.com>
2024-07-22 09:57:14 +0530

    update entry points and minification

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -5,7 +5,13 @@ import svgrPlugin from 'esbuild-plugin-svgr';
 export default defineConfig((options: Options) => ({
     esbuildPlugins: [svgrPlugin(), sassPlugin()],
     treeshake: true,
-    entry: ["./src/index.ts"],
+    minify: true,
+    clean: true,
+    entry: [
+        "./src/index.ts", // import { Card } from "@repo/ui/atoms/Card";
+        "./src/atoms/index.ts", // import { Icons, IconSize } from "@repo/ui/atoms";
+        "./src/atoms/**/index.ts" // import { Icon } from "@repo/ui/atoms/Icon";
+    ],
     format: ["cjs", "esm"],
     dts: true,
     external: ["react"],
