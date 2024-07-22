8d5d27b2f99283dd0ffa8f939731ea2d74fbcf9f
Rahul Yadav <rahul.yadav@codinova.com>
2024-07-23 00:59:41 +0530

    update tsup config to output styles from scss

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -2,12 +2,24 @@ import { defineConfig, type Options } from "tsup";
 import { sassPlugin, postcssModules } from 'esbuild-sass-plugin';
 import svgrPlugin from 'esbuild-plugin-svgr';
 
+
+const styleModule = (cssText: string, nonce?: string) => `\
+const css = \`${cssText.replace(/([$`\\])/g, '\\$1')}\`;
+if(css && typeof document !== 'undefined') {
+    const style = document.createElement("style");
+    if(${nonce}) {
+        style.setAttribute("nonce", ${nonce});
+    }
+    style.appendChild(document.createTextNode(css));
+    document.head.appendChild(style);
+}
+export {css};
+`;
+
 export default defineConfig((options: Options) => ({
     esbuildPlugins: [svgrPlugin(), sassPlugin({
-        transform: postcssModules({
-            scopeBehaviour: "global"
-            // ...put here the options for postcss-modules: https://github.com/madyankin/postcss-modules
-        })
+        type: styleModule,
+        style: 'compressed'
     })],
     treeshake: true,
     minify: true,
@@ -15,11 +27,13 @@ export default defineConfig((options: Options) => ({
     entry: [
         "./src/index.ts", // import { Card } from "@repo/ui/atoms/Card";
         "./src/atoms/index.ts", // import { Icons, IconSize } from "@repo/ui/atoms";
-        "./src/atoms/**/index.ts" // import { Icon } from "@repo/ui/atoms/Icon";
+        "./src/atoms/Card/index.ts" // import { Icon } from "@repo/ui/atoms/Icon";
     ],
-    format: ["cjs", "esm"],
+    format: ['cjs', 'esm'],
     dts: true,
-    external: ["react"],
+    external: ['react'],
+    splitting: false,
+    sourcemap: false,
     injectStyle: true,
     ...options,
 }));
