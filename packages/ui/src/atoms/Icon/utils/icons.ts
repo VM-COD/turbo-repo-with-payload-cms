4088f1aa1dbf94080e554feb329a3a623462b85c
Rahul Yadav <rahul.yadav@codinova.com>
2024-07-24 20:36:06 +0530

    include syu dark logo component

--- packages/ui/src/atoms/Icon/utils/icons.ts
+++ packages/ui/src/atoms/Icon/utils/icons.ts
@@ -1,8 +1,10 @@
 import { Icons } from '../models/model';
 import BulbIcon from '../components/BulbIcon';
+import SyuDarkLogoIcon from '../components/SyuDarkLogoIcon';
 
 const SyuIcons = {
   [Icons.BULB_ICON]: BulbIcon,
+  [Icons.SYU_DARK_LOGO]: SyuDarkLogoIcon,
 };
 
 export default SyuIcons;
